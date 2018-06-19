418d03d3f7ea207806d7418b96666316059e22fe
Jordi Moraleda <moraleda@uniclau.com>
2018-06-19 03:01:01 +0200

    WIP Refactoring the control logic

--- src/index.js
+++ src/index.js
@@ -2,170 +2,185 @@ import React from "react";
 import RootSiblings from "@bam.tech/react-native-root-siblings";
 
 import Toast from "../lib/toast";
-import Notification from "../lib/notification";
+// import Notification from "../lib/notification";
 import Loading from "../lib/loading";
 
-var messageQueue = [];
-var defaults = {
-  duration: 4000,
-  animation: 250
+const TOAST_DURATION = 4000;
+const ANIMATION_DURATION = 200;
+
+const toastQueue = [];
+const notificationQueue = [];
+
+const currentToast = {
+  timeout: null,
+  hidingTimeout: null,
+  view: null
+};
+const currentNotification = {
+  timeout: null,
+  view: null
+};
+const currentLoading = {
+  active: false,
+  hidingTimeout: null,
+  view: null
 };
 
-// Public functions
+// PUBLIC FUNCTIONS
 
-export function show(display = "toast", message, title, options = {}) {
-  if (["toast", "notification", "loading"].indexOf(display) < 0)
-    throw new Error("Invalid display type");
-  else if (typeof message != "string" && display != "loading")
-    throw new Error("The message must be a string");
-  else if (title && typeof title != "string")
-    throw new Error("The title must be a string or null");
+// TOAST
 
-  options = Object.assign({}, defaults, options);
-
-  const idx = Date.now();
-  messageQueue.push({
-    idx,
-    visible: true,
-    display,
-    message,
-    title,
-    options,
-    view: null // will be set when displayed
-  });
-
-  if (messageQueue.length == 1) displayCurrent();
-  return idx;
-}
-
-export function showToast(message, options = {}) {
-  return show("toast", message, null, options); // return idx
-}
-
-export function showNotification(message, title, options) {
-  if (typeof options == "undefined" && typeof title == "object"){
-    options = title;
-    title = null;
+export function showToast(payload) {
+  var text, params;
+  if (!payload) return;
+  else if (typeof payload === "object") {
+    text = payload.text;
+    payload.text = undefined;
+    params = payload;
   }
-  return show("notification", message, title, options); // return idx
+  else if (typeof payload === "string") {
+    text = payload;
+    params = undefined;
+  }
+  else throw new Error("Invalid parameters");
+
+  // nothing shown or something about to hide
+  if (!currentToast.active || currentToast.hidingTimeout) {
+    // abort hiding if necessary
+    if (currentToast.hidingTimeout) {
+      clearTimeout(currentToast.hidingTimeout);
+      currentToast.hidingTimeout = null;
+    }
+    currentToast.text = text;
+    currentToast.params = params;
+    currentToast.active = true;
+
+    // update/show the view
+    if (currentToast.view) {
+      currentToast.view.update(
+        <Toast {...currentToast.params} visible={true} message={currentToast.text} />
+      );
+    }
+    else {
+      currentToast.view = new RootSiblings(
+        <Toast {...currentToast.params} visible={true} message={currentToast.text} />
+      );
+    }
+
+    // Done timeout
+    if (currentToast.timeout) clearTimeout(currentToast.timeout);
+    currentToast.timeout = setTimeout(doneToast, payload.duration || TOAST_DURATION);
+  }
+  // queue the message
+  else {
+    toastQueue.push({
+      text,
+      params
+    });
+  }
 }
 
-export function showLoading(message, title, options) {
-  if (typeof options == "undefined" && typeof title == "object"){
-    options = title;
-    title = null;
+function doneToast() {
+  if (toastQueue.length) {
+    const current = toastQueue.splice(0, 1)[0];
+    const {text, params} = current;
+    currentToast.text = text;
+    currentToast.params = params;
+    currentToast.active = true;
+
+    // update/show the view
+    if (currentToast.view) {
+      currentToast.view.update(
+        <Toast {...currentToast.params} visible={true} message={currentToast.text} />
+      );
+    }
+    else {
+      currentToast.view = new RootSiblings(
+        <Toast {...currentToast.params} visible={true} message={currentToast.text} />
+      );
+    }
+
+    // Done timeout
+    if (currentToast.timeout) clearTimeout(currentToast.timeout);
+    currentToast.timeout = setTimeout(doneToast, currentToast.params && currentToast.params.duration || TOAST_DURATION);
   }
-  return show("loading", message, title, options); // return idx
+  else {
+    currentToast.timeout = null;
+    hideToast();
+  }
+}
+
+function hideToast() {
+  currentToast.active = false;
+  if (currentToast.hidingTimeout) return;
+
+  currentToast.view.update(
+    <Toast {...currentToast.params} visible={false} message={currentToast.text} />
+  );
+  currentToast.hidingTimeout = setTimeout(() => {
+    currentToast.view.destroy();
+    currentToast.view = null;
+    currentToast.hidingTimeout = null;
+  }, ANIMATION_DURATION);
+}
+
+// NOTIFICATION
+
+export function showNotification({ title, text, ...params }) {
+}
+
+// LOADING SPINNER
+
+export function showLoading(payload) {
+  if (!payload) return;
+  else if (typeof payload === "object") {
+    currentLoading.title = title;
+    currentLoading.text = text;
+    payload.title = payload.text = undefined;
+    currentLoading.params = payload;
+  }
+  else if (typeof payload === "string") {
+    currentLoading.title = null;
+    currentLoading.text = text;
+    currentLoading.params = undefined;
+  }
+  else throw new Error("Invalid parameters");
+
+  currentLoading.active = true;
+
+  // Skip hiding if scheduled
+  if (currentLoading.hidingTimeout) {
+    clearTimeout(currentLoading.hidingTimeout);
+    currentLoading.hidingTimeout = null;
+  }
+
+  // update/show the view
+  if (currentLoading.view) {
+    currentLoading.view.update(
+      <Loading {...currentLoading.params} visible={true} message={currentLoading.text} title={currentLoading.title} />
+    );
+  }
+  else {
+    currentLoading.view = new RootSiblings(
+      <Loading {...currentLoading.params} visible={true} message={currentLoading.text} title={currentLoading.title} />
+    );
+  }
 }
 
 export function hideLoading() {
-  for (let i = 0; i < messageQueue.length; i++) {
-    if ("loading" != messageQueue[i].display) continue;
+  currentLoading.active = false;
+  if (currentLoading.hidingTimeout) return;
 
-    if (i == 0) {
-      // tell it to start hiding
-      messageQueue[i].visible = false;
-      updateCurrent();
-
-      setTimeout(() => showNext());
-    } else messageQueue.splice(i, 1);
-    return;
-  }
+  currentLoading.view.update(
+    <Loading {...currentLoading.params} visible={false} message={currentLoading.text} title={currentLoading.title} />
+  );
+  currentLoading.hidingTimeout = setTimeout(() => {
+    currentLoading.view.destroy();
+    currentLoading.view = null;
+    currentLoading.hidingTimeout = null;
+  }, ANIMATION_DURATION);
 }
 
-export function hide(idx) {
-  for (let i = 0; i < messageQueue.length; i++) {
-    if (idx != messageQueue[i].idx) continue;
-
-    if (i == 0) {
-      // tell it to start hiding
-      messageQueue[i].visible = false;
-      updateCurrent();
-
-      setTimeout(() => showNext());
-    } else messageQueue.splice(i, 1);
-    return;
-  }
-}
-
-// Manage
-
-function displayCurrent() {
-  var current = messageQueue[0];
-  const waitTime = current.options.animation + current.options.duration;
-  if (!current) return;
-
-  switch (current.display) {
-    case "toast":
-      current.view = new RootSiblings(
-        (
-          <Toast {...current.options} visible={current.visible} message={current.message} />
-        )
-      );
-      if(current.options.duration) setTimeout(showNext, waitTime);
-      break;
-
-    case "notification":
-      current.view = new RootSiblings(
-        (
-          <Notification {...current.options} visible={current.visible} message={current.message} title={current.title} />
-        )
-      );
-      if(current.options.duration) setTimeout(showNext, waitTime);
-      break;
-
-    case "loading":
-      current.view = new RootSiblings(
-        (
-          <Loading {...current.options} visible={current.visible} message={current.message} title={current.title} />
-        )
-      );
-      break;
-
-    default:
-      return;
-  }
-}
-
-function updateCurrent() {
-  var current = messageQueue[0];
-  if (!current) return null;
-
-  switch (current.display) {
-    case "toast":
-      current.view.update(
-        <Toast {...current.options} visible={current.visible} message={current.message} />
-      );
-      break;
-
-    case "notification":
-      current.view.update(
-        <Notification {...current.options} visible={current.visible} message={current.message} title={current.title} />
-      );
-      break;
-
-    case "loading":
-      current.view.update(
-        <Loading {...current.options} visible={current.visible} message={current.message} title={current.title} />
-      );
-      break;
-
-    default:
-      return null;
-  }
-}
-
-function showNext() {
-  var current = messageQueue[0];
-  if (current && current.view && current.view instanceof RootSiblings) {
-    // tell it to start hiding
-    current.visible = false;
-    updateCurrent();
-
-    // destroy after animation has hidden it
-    setTimeout(() => current.view.destroy(), current.options.animation);
-  }
-
-  messageQueue.splice(0, 1);
-  if (messageQueue.length) displayCurrent();
+export function isLoading() {
+  return currentLoading.active;
 }
